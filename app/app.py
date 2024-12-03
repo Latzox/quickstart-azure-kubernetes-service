1d723bdce7bfdfc6821398c13a90cdf7d630b5ce
Marco Platzer <latzox@outlook.com>
2024-12-03 10:49:35 +0000

    Add test change

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ container_id = os.getenv("HOSTNAME", "Unknown Container")
 
 @app.route("/")
 def home():
-    return f"Hello, AKS! This is a Python container app served by container ID: {container_id}"
+    return f"Hello, Community! This is a Python container app served by container ID: {container_id}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
